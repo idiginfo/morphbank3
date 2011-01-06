1919c5181cf44ede37ebdce329d7831b7a4f9d7b
rbruhn <bruhnrp@gmail.com>
2011-01-06 20:04:30 +0000

    Updated to changes in test svn branch. Fixed quotes in form elements.

--- www/includes/forms.inc.php
+++ www/includes/forms.inc.php
@@ -19,28 +19,28 @@ function getFormField($label, $title, $name, $type, $size, $value, $required,$ti
 function getFormFieldInput($name, $type, $size, $value, $required, $title=null){
 	$tag .= '<input type="'.$type.'" name="'.$name.'"';
 	if($required){
-		$tag.= " class=\"required\"";
+		$tag.= ' class="required"';
 	}
 	if (!empty($value))$tag .= ' value ="'.$value.'"';
 	if (!empty($size)) $tag .= ' size="'.$size.'"';
 	if (!empty($title)) $tag .= ' title="'.$title.'"';
-	$tag .= " />";
+	$tag .= ' />';
 	return $tag;
 }
 
 function getFormHiddenField($name, $value){
-	$tag = "<input type=\"hidden\" name=\"$name\" value=\"$value\">";
+	$tag = '<input type="hidden" name="'.$name.'" value="'.$value.'">';
 	return $tag;
 }
 
 function getFormPasswordField($required = false){
 	$req = getRequired($required);
 	$new = $required === true ? '' : 'New';
-	$tag ='<tr><td><b>'.$new.' Password '.$req.'</b></td>';
+	$tag ='<tr><td><b>'.$new.' Password:</b> '.$req.'</td>';
 	$tag .= '<td><input type="password" id="pin" name="pin" /></td></tr>';
-	$tag .= '<tr><td><b>Confirm Password '.$req.'</b></td>';
+	$tag .= '<tr><td><b>Confirm Password:</b> '.$req.'</td>';
 	$tag .= '<td><input type="password" id="confirm_pin" name="confirm_pin"/></td>';
-	$tag .= "</tr>";
+	$tag .= '</tr>';
 	return $tag;
 }
 
@@ -51,11 +51,11 @@ function getGroupSelect($label, $name, $userId, $prefGroup, $required){
 	$result = $db->query($query);
 	$tag = '<tr>';
 	$tag .= getFormLabel($label, $required);
-	$tag .= '<td><select name="'.$name.'" >';
+	$tag .= '<td><select name="'.$name.'">';
 	while($row = $result->fetchRow(MDB2_FETCHMODE_ASSOC)){
-		$tag .= '<option value="' . $row['id'];
+		$tag .= '<option value="' . $row['id'] . '"';
 		if ($prefGroup == $row['id']) $tag .= ' selected="selected" ';
-		$tag .= '">' . $row['groupname'] . '</option>';
+		$tag .= '>' . $row['groupname'] . '</option>';
 	}
 	$tag .= '</select></td>';
 	$tag .= '</tr>';
@@ -116,7 +116,7 @@ function getStateSelector($label, $title, $name, $value, $required) {
 	$tag .= getFormLabel($label, $required);
 	$tag .= '<td><select name="'.$name.'" title="'.$title.'">'."\n";
 	foreach ($states as $sym => $state){
-		$tag .= '<option value="'.$sym.'" ';
+		$tag .= '<option value="'.$sym.'"';
 		if ($sym == $value) $tag .= ' selected="selected" ';
 		$tag .= '>'. $state . '</option>'."\n";
 	}
