e18923abd15b69efbc9abc89d7be47f60bda3e63
rbruhn <bruhnrp@gmail.com>
2011-01-07 18:47:11 +0000

    Changed about.html to about.php so $config->appName could be used.

---
+++ configuration/content/about.php
@@ -0,0 +1,64 @@
+<h1>About <?php echo $config->appName ?></h1>
+<img src="/style/webImages/blueHR-trans.png" width="450" height="5" class="blueHR" alt="" /> 
+<center>
+<a href="/?id=139881" title="Geranium flowers"><img src="/?id=139881&amp;imgType=thumb" height="70"/></a>
+<a href="/?id=72212" title="Gall wasp larvae"><img src="/?id=72212&amp;imgType=thumb" height="80"/></a>
+<a href="/?id=121630" title="Fish from Bishop Museum collection"><img src="/?id=121630&amp;imgType=jpg" height="80" /></a>
+<a href="/?id=104868" title="Hadrosaur skull"><img src="/?id=104868&amp;imgType=thumb" height="70" /></a>
+</center>
+
+<br />
+<br />
+<b><?php echo $config->appName ?></b> is a continuously growing database of images that scientists use for international collaboration, research 
+and education.  Images deposited in <?php echo $config->appName ?> document a wide variety of research including: specimen-based research in 
+comparative anatomy, morphological phylogenetics, taxonomy and related fields focused on increasing our knowledge about 
+biodiversity. The project receives its main funding from the Biological Databases and Informatics program of the National 
+Science Foundation (<a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0446224" target=blank>Grant DBI-0446224</a>). 
+<br /><br />
+<?php echo $config->appName ?> was established in 1998 by a Swedish-Spanish-American group of entomologists and is currently housed at the 
+School of Computational Science (SCS) at Florida State University.  The project has grown immensely since its beginnings 
+and presently includes a team of 15 biologists, computer scientists and information scientists who are working on developing 
+the software.  <?php echo $config->appName ?> is dedicated to using open-source software and is a Fair Use Web Site.  The software used in the 
+current <?php echo $config->appName ?> system includes <a href="http://www.php.net/" target="blank">PHP</a>, 
+<a href="http://www.imagemagick.org/" target="blank">ImageMagick</a>, 
+<a href="http://www.mysql.com/" target="blank">MySQL</a>, 
+<a href="http://www.apache.org/" target="blank">Apache</a>, 
+<a href="http://www.java.sun.com/" target="blank">Java</a>, and 
+<a href="http://www.mozilla.org/js/" target="blank">JavaScript</a>.
+<br /><br />
+Overall, <?php echo $config->appName ?> facilitates research efforts by making it possible to store, discuss and share detailed images of 
+specimens from all over the world. Currently, <?php echo $config->appName ?> holds more than 216,000 public images of more than 4500 different 
+species.  Many more images are in the system that are presently not public, but are held private until the contributing 
+scientists are ready to release the images.  These images to document characters and features to describe organisms, make 
+observations about the organisms, or make a comment about the images themselves. The set of morphological features unique 
+to a specimen can be used to identify future specimens.  New species are discovered and described by looking at images in 
+<?php echo $config->appName ?> and extinct or very rare specimens are preserved for current and future generations to study.
+<br /><br />
+<?php echo $config->appName ?> is an excellent source of information for identification of biological specimens.  In taxonomy, descriptions of 
+new species or other nomenclatural acts can be documented by images and image comment tools in <?php echo $config->appName ?>. Biologists who 
+use <?php echo $config->appName ?> can mark an image, make comments about images that are seen by other researchers often continents away.  
+<?php echo $config->appName ?> provides stable Web links to image collections for use in online and traditional print publications.  Large 
+biological inventory projects can use <?php echo $config->appName ?> to share image information about morphological types and allow experts to 
+do remote identification.
+<br /><br />
+Beginning with <?php echo $config->appName ?> version 2.7 character and character information can now be recorded using a specific type of 
+collection.  Users can create character states and use the collection slide-sorting software to illustrate characters or 
+as a tool to effectively use images for character discovery.  The ability to create these characters exist now in the system 
+and will soon be able to use these in matrices, and export in Nexus format. 
+<br /><br />
+Part of the <?php echo $config->appName ?> mission to promote sharing of information in the biological community with the public but also with 
+other biologists by creating a set of Web tools to make this easier.  A public outreach collaboration,
+<a href="http://marybrogan.morphbank.net" target="blank"> <?php echo $config->appName ?> at the Mary Brogan</a>, is ongoing with the Mary 
+Brogan Museum of Art and Science to explore the public implications and utility of a educational tool like <?php echo $config->appName ?>.  
+<br /><br />
+<?php echo $config->appName ?> is working towards <a href="/About/Collaborations/">collaborating</a> 
+with many research projects to develop a better community resource. A few of these collaborators include: 
+Herbarium at the University of Alaska Museum of the North, Peabody Museum of Natural History, Systematic Entomology 
+Laboratory  USDA-ARS, Robert K. Godfrey Herbarium, PlantCollections Project, and many National Science Foundation 
+Assembling the Tree of Life (ATOL), Partnership for Enhancing Expertise in Taxonomy (PEET) and Planetary Biodiversity 
+Inventory (PBI) projects.  Our 
+<a href="/myCollection/?id=142482"><?php echo $config->appName ?> image diversity collection</a> highlights these projects 
+and many more.  
+<br /><br />
+We welcome new projects and individual biologists. Click <a href="/Admin/User/new"> here </a> to 
+sign up for an new account and become part of the <?php echo $config->appName ?> project.
