64ab4447a9407970a257512b83140b597829b12d
rbruhn <bruhnrp@gmail.com>
2011-01-10 16:36:31 +0000

    Added copyright. Changed site diabled message.

--- www/includes/showFunctions.inc.php
+++ www/includes/showFunctions.inc.php
@@ -1,4 +1,24 @@
 <?php
+/**
+* Copyright (c) 2011 Greg Riccardi, Fredrik Ronquist.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+* 
+* Contributors:
+*   Fredrik Ronquist - conceptual modeling and interaction design
+*   Austin Mast - conceptual modeling and interaction design
+*   Greg Riccardi - initial API and implementation
+*   Wilfredo Blanco - initial API and implementation
+*   Robert Bruhn - initial API and implementation
+*   Christopher Cprek - initial API and implementation
+*   David Gaitros - initial API and implementation
+*   Neelima Jammigumpula - initial API and implementation
+*   Karolina Maneva-Jakimoska - initial API and implementation
+*   Katja Seltmann - initial API and implementation
+*   Stephen Winner - initial API and implementation
+*/
 
 include ('coordValue.php');
 include_once ('urlFunctions.inc.php');
@@ -601,7 +621,7 @@ function getNonAuthMessage($code) {
 			$msg = 'There is no object with this id';
 			break;
 		case 7: // Site disabled. Only viewing allowed
-		    $msg = 'The site has been temporarily disbled for additions or edits. Only viewing is enabled.';
+		    $msg = 'Site updates have been temporarily suspended while maintenance is performed.';
 		    break;
 		case 8:
 		default: // Unexpected result
