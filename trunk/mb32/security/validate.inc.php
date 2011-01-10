b66a079442da3c60f9029d228cce0e39d99b8a0f
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-01-10 16:37:15 +0000

    Added copyright. Fixed string equivalent for site disabled check.

--- trunk/mb32/security/validate.inc.php
+++ trunk/mb32/security/validate.inc.php
@@ -1,4 +1,25 @@
 <?php
+/**
+* Copyright (c) 2011 Greg Riccardi, Fredrik Ronquist.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+* 
+* Contributors:
+*   Fredrik Ronquist - conceptual modeling and interaction design
+*   Austin Mast - conceptual modeling and interaction design
+*   Greg Riccardi - initial API and implementation
+*   Wilfredo Blanco - initial API and implementation
+*   Robert Bruhn - initial API and implementation
+*   Christopher Cprek - initial API and implementation
+*   David Gaitros - initial API and implementation
+*   Neelima Jammigumpula - initial API and implementation
+*   Karolina Maneva-Jakimoska - initial API and implementation
+*   Katja Seltmann - initial API and implementation
+*   Stephen Winner - initial API and implementation
+*/
+
 /*
  File name: validate.inc.php
  @author Neelima Jammigumpula <jammigum@scs.fsu.edu>
@@ -64,7 +85,7 @@ function checkAuthorization($objectId, $userId = null, $groupId = null, $functio
 	if ($groupId == $config->adminGroup) return true;
 	
 	// Disable everything but viewing if config->disableSite value set
-	if ($function == 'view' && $config->disableSite == 1) {
+	if ($function != 'view' && $config->disableSite == 1) {
 	  $nonAuthCode = 7;
 	  return false;
 	}
