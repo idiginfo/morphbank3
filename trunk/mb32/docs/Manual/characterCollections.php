3e7696af6128a22a90089887441895023c2c68b2
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-01-10 17:17:47 +0000

    Fixed copyright to be within php tags. Removed global variable definitions. Changed $domainName to $config->domain.

--- trunk/mb32/docs/Manual/characterCollections.php
+++ trunk/mb32/docs/Manual/characterCollections.php
@@ -1,8 +1,27 @@
-
 <?php 
-	//global $includeDirectory, $dataDirectory, $imgDirectory;
+/**
+* Copyright (c) 2011 Greg Riccardi, Fredrik Ronquist.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+* 
+* Contributors:
+*   Fredrik Ronquist - conceptual modeling and interaction design
+*   Austin Mast - conceptual modeling and interaction design
+*   Greg Riccardi - initial API and implementation
+*   Wilfredo Blanco - initial API and implementation
+*   Robert Bruhn - initial API and implementation
+*   Christopher Cprek - initial API and implementation
+*   David Gaitros - initial API and implementation
+*   Neelima Jammigumpula - initial API and implementation
+*   Karolina Maneva-Jakimoska - initial API and implementation
+*   Katja Seltmann - initial API and implementation
+*   Stephen Winner - initial API and implementation
+*/
+
 	
-	include_once($_SERVER['DOCUMENT_ROOT'] . '/includes/head.inc.php');
+	include_once('head.inc.php');
 	$title =  'About - Manual';
 	initHtml( $title, NULL, NULL);
 	echoHead( false, $title);
