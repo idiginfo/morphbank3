58c85f82c5a99f6fcf8146d2b1573ea1e8b8df38
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-01-10 19:52:28 +0000

    Used $config->appName where site name is used

--- trunk/mb32/configuration/content/citation.php
+++ trunk/mb32/configuration/content/citation.php
@@ -1,43 +1,20 @@
-<?php 
-/**
-* Copyright (c) 2011 Greg Riccardi, Fredrik Ronquist.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Public License v2.0
-* which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-* 
-* Contributors:
-*   Fredrik Ronquist - conceptual modeling and interaction design
-*   Austin Mast - conceptual modeling and interaction design
-*   Greg Riccardi - initial API and implementation
-*   Wilfredo Blanco - initial API and implementation
-*   Robert Bruhn - initial API and implementation
-*   Christopher Cprek - initial API and implementation
-*   David Gaitros - initial API and implementation
-*   Neelima Jammigumpula - initial API and implementation
-*   Karolina Maneva-Jakimoska - initial API and implementation
-*   Katja Seltmann - initial API and implementation
-*   Stephen Winner - initial API and implementation
-*/
-
-?>
-<h1>Morphbank Copyright Policy</h1>
+<h1><? echo $config->appName; ?> Copyright Policy</h1>
 <img src="/style/webImages/blueHR-trans.png" width="525" height="5" class="blueHR" alt="" />
 <p>
-Morphbank is an open web repository of images serving the biological research community.  Morphbank is designated as 
-a Fair Use Web Site.  The objective of Morphbank is not to reward the labor of authors, but to promote the progress of 
-science. The images in Morphbank that are not password protected can be used for private, education, research or other 
+<? echo $config->appName; ?> is an open web repository of images serving the biological research community.  <? echo $config->appName; ?> is designated as 
+a Fair Use Web Site.  The objective of <? echo $config->appName; ?> is not to reward the labor of authors, but to promote the progress of 
+science. The images in <? echo $config->appName; ?> that are not password protected can be used for private, education, research or other 
 non-commercial purposes for free, provided that the source and the copyright holder are cited. Any commercial use 
-requires consent from the copyright holder. The images in Morphbank that are password protected are considered 
+requires consent from the copyright holder. The images in <? echo $config->appName; ?> that are password protected are considered 
 "work in-progress" and are not released to the public.  These images may not be used without specific written 
-authorization from the copyright holder. Contributors to Morphbank agree to these terms.
+authorization from the copyright holder. Contributors to <? echo $config->appName; ?> agree to these terms.
 </p>
 <p>
-Morphbank, by default, uses <a href="http://creativecommons.org/">Creative Commons</a> (cc) <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">3.0 license (BY-NC-SA)</a>. 
+<? echo $config->appName; ?>, by default, uses <a href="http://creativecommons.org/">Creative Commons</a> (cc) <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">3.0 license (BY-NC-SA)</a>. 
 Contributors may opt to upload their images with <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">cc 3.0</a>, 
 a <em>less restrictive</em> license or Public Domain. Choosing the appropriate license is easy from <a href="http://creativecommons.org/choose/">Creative Commons - License Your Work</a>
 </p>
 <br/>
-<h1>How to cite Morphbank</h1>
-<p><strong>Morphbank (http://www.morphbank.net, <?php echo date('d F Y') ?>).
+<h1>How to cite <? echo $config->appName; ?></h1>
+<p><strong><? echo $config->appName; ?> (http://<?php echo $config->domain; ?>, <?php echo date('d F Y') ?>).
 Florida State University, Department of Scientific Computing, Tallahassee, FL 32306-4026 USA.<br /></strong></p>
