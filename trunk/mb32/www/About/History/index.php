f1c0bbc41171a5119de6f5532fc159416590060c
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-01-10 20:00:35 +0000

    Used includes for files concerning conent.

--- trunk/mb32/www/About/History/index.php
+++ trunk/mb32/www/About/History/index.php
@@ -29,9 +29,9 @@ initHtml( $title, NULL, NULL);
 // Add the standard head section to all the HTML output.
 echoHead( false, $title);
 
-$historyHtml = file_get_contents('content/history.html', true);
-// Output the content of the main frame
-echo '<div class="mainGenericContainer" >'.$historyHtml.'</div>';
+echo '<div class="mainGenericContainer" >';
+include('content/history.php');
+echo '</div>';
 
 // Finish with end of HTML
 finishHtml();
