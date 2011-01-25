989329509e139f99cd268b5c64dff97cb4b67bc2
rbruhn <bruhnrp@gmail.com>
2011-01-25 21:11:38 +0000

    Changed split() to either preg_split() or explode() where appropriate. split() in php 5.3 is deprecated.

--- www/ie7/ie7-base64.php
+++ www/ie7/ie7-base64.php
@@ -1,7 +1,7 @@
 <?php
-$data = split(";", $_SERVER["REDIRECT_QUERY_STRING"]);
+$data = explode(";", $_SERVER["REDIRECT_QUERY_STRING"]);
 $type = $data[0];
-$data = split(",", $data[1]);
+$data = explode(",", $data[1]);
 header("Content-type: ".$type);
 echo base64_decode($data[1]);
 ?>
