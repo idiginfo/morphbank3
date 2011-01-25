d2481751611cd2aabbd8f7401534c4f7a0589b11
rbruhn <bruhnrp@gmail.com>
2011-01-25 17:29:35 +0000

    Testing is_writable() function

--- www/Admin/User/test.php
+++ www/Admin/User/test.php
@@ -1,5 +1,13 @@
 <?php
+/*
 if (is_writable($config->userLogoPath)) {
+  echo "writable";
+} else {
+  echo "not writable";
+}
+*/
+
+if (is_writable('/images/userLogos')) {
   echo "writable";
 } else {
   echo "not writable";
