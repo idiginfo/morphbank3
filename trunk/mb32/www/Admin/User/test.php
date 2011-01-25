eddbd7ed65a6f1989f931a63fe530326bdf009eb
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-01-25 17:48:47 +0000

    Testing is_writable() function

--- trunk/mb32/www/Admin/User/test.php
+++ trunk/mb32/www/Admin/User/test.php
@@ -7,13 +7,13 @@ if (is_writable($config->userLogoPath)) {
 }
 */
 echo $_SERVER['DOCUMENT_ROOT'] . "<br />";
-if (is_dir('/data/mb32testsite/www/images/userLogos/')) {
+if (is_dir($config->userLogoPath)) {
   echo "directory exists<br />";
 } else {
   echo "directory missing<br />";
 }
 
-if (is_writable('/data/mb32testsite/www/images/userLogos/')) {
+if (is_writable($config->userLogoPath)) {
   echo "writable";
 } else {
   echo "not writable";
