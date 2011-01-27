ccfea70269affd533d3f194610af93e3f8417bda
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-01-27 18:26:11 +0000

    Added ability to add Admin user during install. Added check for directories being writable.

--- trunk/mb32/www/install/install_functions.php
+++ trunk/mb32/www/install/install_functions.php
@@ -28,20 +28,20 @@ require_once("MDB2.php");
  */
 $defaultConfigValues = array(
 	'PHP Settings' => array (
-			'phpsettings.display_errors' => 0
+			'display_errors' => 0
 		),
 	'Database' => array (
-			'dsn.param.port'    => 3306,
-			'dsn.param.driver'  => '"mysqli"',
-			'dsn.param.charset' => '"UTF8"',
-			'dsn.param.debug'   => 0
+			'dbPort'    => 3306,
+			'dbDriver'  => '"mysqli"',
+			'dbCharset' => '"UTF8"',
+			'dbDebug'   => 0
 		),
 	'Error handling' => array(
-			'error.logging'  => 1,
-			'error.priority' => 4,
-			'error.redirect' => 1,
-			'error.response' => 5,
-			'error.logFile'  => 'APPLICATION_PATH "/log/application.log"'
+			'errorLogging'  => 1,
+			'errorPriority' => 4,
+			'errorRedirect' => 1,
+			'errorResponse' => 5,
+			'errorLogFile'  => 'APPLICATION_PATH "/log/application.log"'
 		),
 	'Image Server' => array (
 			'imgAppPath'  => 'APPLICATION_PATH "/ImageServer/"',
@@ -136,6 +136,15 @@ function localIp() {
  * Get connection to database
  */
 function dbConnect() {
+    global $db;
+    
+    if ($db){
+		$database = $db->getDatabase();
+		if (!PEAR::isError($database)) {
+			return $db;
+		}
+	}
+	
 	$dsn = array(
 		'phptype'  => 'mysqli',
 		'username' => $_POST['db_user'],
@@ -155,14 +164,58 @@ function dbConnect() {
 }
 
 /**
+ * Checks if CurrentIds table already holds information
+ * @param $type
+ */
+function checkCurrentIds($type) {
+  $db = dbConnect();
+  $sql = "select type from CurrentIds where type = '$type'";
+  $result = $db->query($query);
+  return $result->numRows();
+}
+
+/**
+ * Insert or update CurrentIds table
+ * @param $count
+ * @param $min
+ * @param $max
+ * @param $type
+ */
+function setCurrentIds($count, $min, $max, $type) {
+  $db = dbConnect();
+
+  $data = array($min, $max, $type);
+  $sql = ($count == 0) ? "insert into CurrentIds set minId = ?, maxId = ?, type = ?"
+                          : "update CurrentIds set minId = ?, maxId = ? where type = ?";
+  $stmt = $db->prepare($sql);
+  if (PEAR::isError($stmt)) return $stmt->getUserInfo();
+  $result = $statement->execute($data);
+  if (PEAR::isError($result)) return $result->getUserInfo();
+  $stmt->free();
+  return;
+}
+
+/**
  * Install application
  */
 function installApp() {
 	global $defaultConfigValues;
 	
+	// Check if directories are writable
+	$writable = '';
+	if (!is_writable('/../configuration')) $writable .= "/configuration directory is not writable.<br />";
+	if (!is_writable('/../log')) $writable .= "/log directory is not writable.<br />";
+	if (!is_writable('/../www/images/userLogos')) $writable .= "/www/images/userLogos directory is not writable.<br />";
+    if (!is_writable('/../www/newsImages')) $writable .= "/www/images/newsImages directory is not writable.<br />";
+	if (!is_writable('/../www/images/mirrorLogos')) $writable .= "/www/images/mirrorLogos directory is not writable.<br />";
+	if (!empty($writable)) return $writable;
+	
 	if (!isset($_POST['submit'])) return;
 	
-	// check all post values
+	// check if passwords are the same
+	if ($_POST['pin'] != $_POST['confirm_pin']) return "Admin passwords not the same.";
+	
+	// check all post values for empty values
 	foreach ($_POST as $key => $value) {
 		if ($key == 'db_pass' && empty($value) && localIp()) continue;
 		if ($key == 'db_pass' && empty($value) && !localIp()) return "Database password required.";
@@ -170,9 +223,6 @@ function installApp() {
 	}
 	
 	$db = dbConnect();
-	if (PEAR::isError($db)) {
-		return $db->getUserInfo();
-	}
 	
 	// Open config file for writing
 	$filePath = '../../configuration';
@@ -186,7 +236,7 @@ function installApp() {
 		switch ($section) {
 			case 'PHP Settings':
 				$postArray = array (
-					'phpsettings.date.timezone' => '"'.$_POST['app_timezone'].'"'
+					'timezone' => '"'.$_POST['app_timezone'].'"'
 				);
 				$array = array_merge($postArray, $array);
 				$string = buildConfigSection($section, $array);
@@ -194,11 +244,11 @@ function installApp() {
 				break;
 			case 'Database':
 				$postArray = array (
-					'dsn.param.dbname'   => '"'.$_POST['db_name'].'"',
-					'dsn.param.username' => '"'.$_POST['db_user'].'"',
-					'dsn.param.password' => '"'.$_POST['db_pass'].'"',
-					'dsn.param.hostname' => '"'.$_POST['db_host'].'"',
-					'dsn.param.port'     => $_POST['db_port']
+					'dbName'   => '"'.$_POST['db_name'].'"',
+					'dbUsername' => '"'.$_POST['db_user'].'"',
+					'dbPassword' => '"'.$_POST['db_pass'].'"',
+					'dbHostname' => '"'.$_POST['db_host'].'"',
+					'dbPort'     => $_POST['db_port']
 				);
 				$array = array_merge($postArray, $array);
 				$string = buildConfigSection($section, $array);
@@ -256,9 +306,7 @@ function installApp() {
 	// Set default hostSErver in BaseObject
 	$hostServer = $db->escape($_POST['app_server']);
 	$result = $db->exec("ALTER TABLE `BaseObject` CHANGE `hostServer` `hostServer` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '$hostServer'");
-	if (PEAR::isError($result)) {
-		return $result->getUserInfo();
-	}
+	if (PEAR::isError($result)) return $result->getUserInfo();
 	
 	// Set min and max ids for object and tsn
 	$obj_min_id = $_POST['db_object_min_id'];
@@ -266,32 +314,86 @@ function installApp() {
 	$tsn_min_id = $_POST['db_tsn_min_id'];
 	$tsn_max_id = $tsn_min_id + 499999;
 	
-	// Update min and max id for object
-	$data = array($obj_min_id, $obj_max_id);
-	$sql = "update CurrentIds set minId = ?, maxId = ? where type = 'object'";
-	$stmt = $db->prepare($sql);
-	if (PEAR::isError($stmt)) {
-		return $stmt->getUserInfo();
-	}
-	$result = $statement->execute($data);
-	if (PEAR::isError($result)) {
-		return $result->getUserInfo();
-	}
-	$stmt->free();
+	// Check CurrentIds for object and tsn
+	$obj_count = checkCurrentIds('object');
+	$tsn_count = checkCurrentIds('tsn');
 	
-	// Update min and max id for tsn
-	$data = array($tsn_min_id, $tsn_max_id);
-	$sql = "update CurrentIds set minId = ?, maxId = ? where type = 'tsn'";
-	$stmt = $db->prepare($sql);
-	if (PEAR::isError($stmt)) {
-		return $stmt->getUserInfo();
-	}
-	$result = $statement->execute($data);
-	if (PEAR::isError($result)) {
-		return $result->getUserInfo();
-	}
-	$stmt->free();
+	// Set CurrentIds for object
+	$obj = setCurrentIds($obj_count, $obj_min_id, $obj_max_id, 'object');
+	if (!empty($obj)) return $obj;
 	
+	// Set CurrentIds for tsn
+	$tsn = setCurrentIds($tsn_count, $tsn_min_id, $tsn_max_id, 'tsn');
+	if (!empty($tsn)) return $tsn;
+	
+	// require updater class for following operations
+	require_once('../includes/updater.class.php');
+	
+	// Create new user for admin
+    $first_name   = $_POST['first_name'];
+    $last_name    = $_POST['last_name'];
+    $uin          = $_POST['uin'];
+    $pin          = $_POST['pin'];
+    $email        = $_POST['email'];
+    $affiliation  = $_POST['affiliation'];
+    $country      = $_POST['country'];
+    $name         = trim($first_name) . " " . trim($last_name);
+    
+    // Get Admin user id for default
+    $groupId = 2;
+	$sql = "SELECT min(userId) as userId FROM `BaseObject` WHERE groupId = $groupId";
+	$userId = $db->getOne($sql);
+	if (PEAR::isError($userId)) return $userId->getUserInfo();
+	
+	// Insert BaseObject for User
+    $params = array($db->quote("User"), $userId, $groupId, $userId, "NOW()", $db->quote("User added"), $db->quote(NULL));
+    $stmt = $db->executeStoredProc('CreateObject', $params);
+    if (PEAR::isError($stmt)) return 'Create Object procedure for User: ' . $stmt->getUserInfo();
+    $id = $stmt->fetchOne();
+    while($stmt->nextResult()) $stmt->store_result();
+    
+    // Insert BaseObject for Groups
+    $params = array($db->quote("Groups"), $userId, $groupId, $userId, "NOW()", $db->quote("Group added"), $db->quote(NULL));
+    $stmt = $db->executeStoredProc('CreateObject', $params);
+    if (PEAR::isError($stmt)) return 'Create Object procedure for Groups: ' . $stmt->getUserInfo();
+    $group_id = $stmt->fetchOne();
+    while($stmt->nextResult()) $stmt->store_result();
+	
+    // prepare user update to insert into User Table
+    $userUpdater = new Updater($db, $id, $userId , $groupId, 'User');
+    $userUpdater->addField('last_Name', $last_name, null);
+    $userUpdater->addField('uin', $uin, null);
+    $userUpdater->addPasswordField('pin', $pin, null);
+    $userUpdater->addField('email', $email, null);
+    $userUpdater->addField('status', 1, null);
+    $userUpdater->addField('affiliation', $affiliation, null);
+    $userUpdater->addField('privilegeTSN', 0, null); // TODO Default 0. Eventually remove
+    $userUpdater->addField('primaryTSN', 0, null); // TODO Default 0. Eventually remove
+    $userUpdater->addField('secondaryTSN', 0, null); // TODO Default 0. Eventually remove
+    $userUpdater->addField('country', $country, null);;
+    $userUpdater->addField('name', $name, null);
+    $userUpdater->addField('address', $address, null);
+    $userUpdater->addField("preferredGroup", $group_id, null);
+    $numRows = $userUpdater->executeUpdate();
+    if (is_string($numRows)) return $numRows;
+
+    // prepare Groups update
+    $userUpdater = new Updater($db, $group_id, $userId , $groupId, 'Groups');
+    $userUpdater->addField('groupName', "$uin's group", null);
+    $userUpdater->addField('tsn', 0,null);
+    $userUpdater->addField('groupManagerId', $id, null);
+    $userUpdater->addField('status', 1, null);
+    $userUpdater->addField('dateCreated', $db->mdbNow(), null);
+    $numRows = $userUpdater->executeUpdate();
+    if (is_string($numRows)) return $numRows;
+    
+    // Insert in UserGroup table
+    $data = array($id, $group_id, $userId, $db->mdbNow(), $db->mdbToday(), 'coordinator');
+    $sql = "insert into UserGroup (user, groups, userId, dateCreated, dateToPublish, userGroupRole) values (?,?,?,?,?,?)";
+    $stmt = $db->prepare($sql);
+    $affRows = $stmt->execute($data);
+    if (PEAR::isError($affRows)) return 'Create user group error:' . $affRows->getUserInfo();
+
 	return;
 }
 
