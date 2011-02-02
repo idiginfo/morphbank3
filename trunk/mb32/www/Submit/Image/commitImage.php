dcb5fe5b42bf9ffc3595b6f703f95dacf94649d4
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-02-02 19:47:44 +0000

    Altered check for specimen

--- trunk/mb32/www/Submit/Image/commitImage.php
+++ trunk/mb32/www/Submit/Image/commitImage.php
@@ -63,7 +63,7 @@ if (empty($vid)) {
 // Check for valid specimen id and get locality id for image update
 $sql = "select count(*) as count from Specimen where id = ?";
 $count = $db->getOne($sql, array('integer'), array($specimenId));
-if (isMdb2Error($localityId, "Specimen does not exists.", 5)) {
+if ($count == 0 || isMdb2Error($count, "Specimen does not exists.", 5)) {
 	header("location: $indexUrl&code=3");
 	exit;
 }
