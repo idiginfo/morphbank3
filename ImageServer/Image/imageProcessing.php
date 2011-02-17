942e30ed26137373ee50fb1db72b710e6978a9f4
rbruhn <bruhnrp@gmail.com>
2011-02-17 18:24:23 +0000

    Changed search file source message

--- ImageServer/Image/imageProcessing.php
+++ ImageServer/Image/imageProcessing.php
@@ -186,7 +186,7 @@ function replaceOriginal ($id, $fileAccessPath, $fileName, $fileSourceDir){
 
 function getFileFromFileSystem($fileAccessPath, $fileSourceDir){
 	global $message;
-        $message .= ": searching $fileSourceDir for id";
+        $message .= ": searching $fileSourceDir for image";
 	// try to find file in ftp site
 	$escFileName = str_replace(" ", "\ ", $fileAccessPath);
 	$escFileName = str_replace("[", "\[", $escFileName);
