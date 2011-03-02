a223d35bc6bbfba87cd3e425b07816de1ccfcd0f
griccardi <griccardi@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-03-02 18:27:28 +0000

    rewrote code to conform to MDB2 db strategy

--- trunk/mb32/adminProcesses/standardImageIds.php
+++ trunk/mb32/adminProcesses/standardImageIds.php
@@ -1,91 +1,65 @@
 <?php
 /**
-* Copyright (c) 2011 Greg Riccardi, Fredrik Ronquist.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Public License v2.0
-* which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-* 
-* Contributors:
-*   Fredrik Ronquist - conceptual modeling and interaction design
-*   Austin Mast - conceptual modeling and interaction design
-*   Greg Riccardi - initial API and implementation
-*   Wilfredo Blanco - initial API and implementation
-*   Robert Bruhn - initial API and implementation
-*   Christopher Cprek - initial API and implementation
-*   David Gaitros - initial API and implementation
-*   Neelima Jammigumpula - initial API and implementation
-*   Karolina Maneva-Jakimoska - initial API and implementation
-*   Deborah Paul - initial API and implementation implementation
-*   Katja Seltmann - initial API and implementation
-*   Stephen Winner - initial API and implementation
-*/
+ * Copyright (c) 2011 Greg Riccardi, Fredrik Ronquist.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * Contributors:
+ *   Fredrik Ronquist - conceptual modeling and interaction design
+ *   Austin Mast - conceptual modeling and interaction design
+ *   Greg Riccardi - initial API and implementation
+ *   Wilfredo Blanco - initial API and implementation
+ *   Robert Bruhn - initial API and implementation
+ *   Christopher Cprek - initial API and implementation
+ *   David Gaitros - initial API and implementation
+ *   Neelima Jammigumpula - initial API and implementation
+ *   Karolina Maneva-Jakimoska - initial API and implementation
+ *   Deborah Paul - initial API and implementation implementation
+ *   Katja Seltmann - initial API and implementation
+ *   Stephen Winner - initial API and implementation
+ */
 
 
 require_once(dirname(dirname(__FILE__)) . '/configuration/app.server.php');
-$config->errorRedirect = 0;
+require_once('admin.inc.php');
 
-$link = Adminlogin();
-mysqli_select_db($link, "MB30");
-$sql = "SELECT id FROM Specimen WHERE (standardImageId = 0 OR standardImageId IS NULL) ";
+$db = connect();
 echo date("H:i:s\n");
 
-$results = mysqli_query($link, $sql);
-if ($results) {
-	$numRows = mysqli_num_rows($results);
-	for ($i=0; $i < $numRows; $i++) {
-		$specimenArray[$i] = mysqli_fetch_array($results);
-	}
-	$spCount = 0;
-	for ($i=0; $i < $numRows; $i++) {
-		$sql = 'SELECT Image.id FROM Image WHERE Image.specimenId = '.$specimenArray[$i]['id'].' ORDER BY Image.id ASC LIMIT 0,1 ';
-		//echo $sql."\n";
-		$result = mysqli_query($link, $sql);
-		if ($result) {
-			$idArray= mysqli_fetch_array($result);
-			if ($idArray['id']) {
-				$updateSql = 'UPDATE Specimen SET standardImageId = '.$idArray['id'].' WHERE id = '.$specimenArray[$i]['id'];
-				//echo $updateSql."\n";
-				mysqli_query($link, $updateSql);
-				$updateSql = 'UPDATE BaseObject SET thumbURL = '.$idArray['id'].' WHERE id = '.$specimenArray[$i]['id'];
-				//echo $updateSql."\n";
-				mysqli_query($link, $updateSql);
-				$spCount++;
-			}
-		}
-	}
-}
-
-$sql = "SELECT View.id FROM View WHERE (standardImageId = 0 OR standardImageId IS NULL) ";
-$results = mysqli_query($link, $sql);
-if ($results) {
-	$numRows = mysqli_num_rows($results);
-	for ($i=0; $i < $numRows; $i++) {
-		$viewArray[$i] = mysqli_fetch_array($results);
-	}
-	$viewCount = 0;
-	for ($i=0; $i < $numRows; $i++) {
-		$sql = 'SELECT Image.id FROM Image WHERE Image.viewId = '.$viewArray[$i]['id'].' ORDER BY Image.id ASC LIMIT 0,1 ';
-		//echo $sql."\n";
-		$result = mysqli_query($link, $sql);
-
-		if ($result) {
-			$idArray= mysqli_fetch_array($result);
-			if ($idArray['id']) {
-				$updateSql = 'UPDATE View SET standardImageId = '.$idArray['id'].' WHERE id = '.$viewArray[$i]['id'];
-				//echo $updateSql."\n";
-				mysqli_query($link, $updateSql);
-				$updateSql = 'UPDATE BaseObject SET thumbURL = '.$idArray['id'].' WHERE id = '.$viewArray[$i]['id'];
-				//echo $updateSql."\n";
-				mysqli_query($link, $updateSql);
-				$viewCount++;
-			}
-		}
-	}
-	echo "\n\nTotal Specimens: ".$spCount."\n";
-	echo "\n\nTotal Views: ".$viewCount."\n\n";
-
-}
+$specCount = updateTable('Specimen','specimenId');
+echo "\n\nTotal Specimens: ".$spCount."\n";
+$viewCount = updateTable('View','viewId');
+echo "\n\nTotal Views: ".$viewCount."\n\n";
 echo date("H:i:s\n");
 
-?>
+
+function updateTable($table, $field){
+global $db;
+	$sql = "SELECT id FROM $table WHERE (standardImageId = 0 OR standardImageId IS NULL) ";
+	$results = $db->query($sql);
+	isMdb2Error($results,$sql);
+
+	$objCount = 0;
+	while ($id = $results->fetchOne()) {
+		$sql = "SELECT id FROM Image WHERE $field=$id ORDER BY id ASC LIMIT 1";
+		//echo $sql."\n";
+		$stdId = $db->queryOne($sql);
+		isMdb2Error($stdId,$sql);
+		if (!empty($stdId)){
+			$updateSql = "UPDATE $table SET standardImageId=$stdId WHERE id=$id";
+			//echo $updateSql."\n";
+			$count = $db->exec($updateSql);
+			isMdb2Error($count,$updateSql);
+			$updateSql = "UPDATE BaseObject SET thumbURL=$stdId WHERE id=$id";
+			//echo $updateSql."\n";
+			$count = $db->exec($updateSql);
+			isMdb2Error($count,$updateSql);
+			$objCount++;
+		}
+	}
+	return $objCount;
+}
+
+
