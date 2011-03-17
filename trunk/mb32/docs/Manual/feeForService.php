4dd396c514d08495f0584a02ee5099bc291d802a
dlpaul <dlpaul@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-03-17 19:18:52 +0000

    fixing html

--- trunk/mb32/docs/Manual/feeForService.php
+++ trunk/mb32/docs/Manual/feeForService.php
@@ -45,39 +45,36 @@ This outline helps users decide how they might proceed as would-be contributors 
 Note, each use-case is unique and someone on the Morphbank Staff will evaluate each request 
 and give the contributor a quote up-front.</p>
 
-<ol>
-<li>Upload via Morphbank XML. RATE:  $30.00 / hour</br>
+
+<p>1. Upload via Morphbank XML. RATE:  $30.00 / hour</br>
 This assumes the contributor's existing database has been mapped to Morphbank. 
 See # 2 below.Use this option if the contributor's data is already in a database. 
 NB. Morphbank is building a piece of software to automate data export from Specify6 into Morphbank.
-</li>
+</p>
 
-<li>Mapping to Morphbank. One-Time Fee @ RATE:  $30.00 /  hour</br>
+<p>2. Mapping to Morphbank. One-Time Fee @ RATE:  $30.00 /  hour</br>
 For users with existing databases, mapping is provided at $30.00 / hour and is a one-time fee. 
 The contributor receives a map of the Schema. Hint here to users: the more familiar a would-be 
 contributor is with Morphbank, the faster this process will be. Users may send a sample dataset 
 to get an estimate of how long the mapping might take.
-</li>
+</p>
 
-<li>Upload via Morphbank Excel Workbook - 250 records per workbook	RATE:  $30.00 / hour</br>
+<p>3. Upload via Morphbank Excel Workbook - 250 records per workbook	RATE:  $30.00 / hour</br>
 Suitable for smaller datasets - where the contributor's data is not already databased. 
 Upload of one workbook of this size will take 1 - 2 hours and assumes the contributor does the following:
-</li>
-	<li>
+</p>
 	<ul>
 	<li>Checks all Taxon Names in the dataset with Taxon Names in Morphbank. 
-	Note there is an automated way to do this. Contact mbadmin@scs.fsu.edu to learn how to do this.
+	Note there is an automated way to do this via <a href="/Help/nameMatch/">Name Query</a>. Contact mbadmin@scs.fsu.edu for any needed help with this.
 	</li>
-	<li>Adds any Taxon Names that need adding (via the web-interface) or the Specimen Taxon Data sheet in the Workbook.
+	<li>Adds any Taxon Names that need adding (via the web-interface) or the Specimen Taxon Data sheet in the Excel Workbook.
 	</li>
 	<li>Sends a completed Excel Workbook with no (or only minor) errors.
 	</li>
 	<li>Sends images via ftp, DVD or harddrive.
 	</li>
 	</ul>
-	</li>
 
-	<li>
 	<ul>
 	<li>The Morphbank Staff member will:</li>
 	<li>Check the Excel Workbook, especially Taxon Names &amp; Image file names and return a time / cost 
@@ -86,22 +83,22 @@ Upload of one workbook of this size will take 1 - 2 hours and assumes the contri
 	<li>Upload the data and images from the completed Morphbank Excel Workbook.</li>
 	<li>Return a report to the Contributor and / or point the contributor to http://services.morphbank.net/mb3/</li> 
 	</ul>
-	</li>
 
-<li>Morphbank Helpdesk &amp; User Feedback Response. RATE:  $30.00 / hour</br>
+
+<p>4. Morphbank Helpdesk &amp; User Feedback Response. RATE:  $30.00 / hour</br>
 Time will be accrued &amp; billing will take place on a to-be-agreed-upon schedule (quarterly, perhaps).
-</li>
+</p>
 
-<li>One-time Infrastructure / Equipment Fee		   One-Time Fee @ RATE:  $1.00 / GB</br>
+<p>5. One-time Infrastructure / Equipment Fee		   One-Time Fee @ RATE:  $1.00 / GB</br>
 To defray the costs of equipment, a one-time fee based on the GB total for the images to be uploaded to Morphbank.
-</li>
+</p>
 
-<li>Overhead - Subcontract or Consultant</br>
+<p>6. Overhead - Subcontract or Consultant</br>
 Subcontract with FSU for &amp; Morphbank has to pay overhead of 46%. 
 One may pay our support person directly for support with lower overhead on our side. 
 We can do this at 20%. Contact riccardi@ci.fsu.edu for an estimate.
-</li>
-</ol>
+</p>
+
 			<br />
 			<div id=footerRibbon></div>
 			<table align="right">
