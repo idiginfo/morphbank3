4aa736f1783027dd080e6d3182d73a4bbb24dffb
cmonnom <cmonnom@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-05-24 17:16:53 +0000

    added php anchors to fix broken variable

--- configuration/content/citation.php
+++ configuration/content/citation.php
@@ -1,20 +1,22 @@
-<h1><? echo $config->appName; ?> Copyright Policy</h1>
+<h1><?php echo $config->appName; ?> Copyright Policy</h1>
 <img src="/style/webImages/blueHR-trans.png" width="525" height="5" class="blueHR" alt="" />
 <p>
-<? echo $config->appName; ?> is an open web repository of images serving the biological research community.  <? echo $config->appName; ?> is designated as 
-a Fair Use Web Site.  The objective of <? echo $config->appName; ?> is not to reward the labor of authors, but to promote the progress of 
-science. The images in <? echo $config->appName; ?> that are not password protected can be used for private, education, research or other 
+<?php echo $config->appName; ?> is an open web repository of images serving the biological research community.  <?php echo $config->appName; ?> is designated as 
+a Fair Use Web Site.  The objective of <?php echo $config->appName; ?> is not to reward the labor of authors, but to promote the progress of 
+science. The images in <?php echo $config->appName; ?> that are not password protected can be used for private, education, research or other 
 non-commercial purposes for free, provided that the source and the copyright holder are cited. Any commercial use 
-requires consent from the copyright holder. The images in <? echo $config->appName; ?> that are password protected are considered 
+requires consent from the copyright holder. The images in <?php echo $config->appName; ?> that are password protected are considered 
 "work in-progress" and are not released to the public.  These images may not be used without specific written 
-authorization from the copyright holder. Contributors to <? echo $config->appName; ?> agree to these terms.
+authorization from the copyright holder. Contributors to <?php echo $config->appName; ?> agree to these terms.
 </p>
 <p>
-<? echo $config->appName; ?>, by default, uses <a href="http://creativecommons.org/">Creative Commons</a> (cc) <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">3.0 license (BY-NC-SA)</a>. 
+<?php echo $config->appName; ?>, by default, uses <a href="http://creativecommons.org/">Creative Commons</a> (cc) <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">3.0 license (BY-NC-SA)</a>. 
 Contributors may opt to upload their images with <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">cc 3.0</a>, 
 a <em>less restrictive</em> license or Public Domain. Choosing the appropriate license is easy from <a href="http://creativecommons.org/choose/">Creative Commons - License Your Work</a>
 </p>
 <br/>
-<h1>How to cite <? echo $config->appName; ?></h1>
-<p><strong><? echo $config->appName; ?> (<?php echo $config->appServerBaseUrl; ?>, <?php echo date('d F Y') ?>).
+<h1>How to cite <?php echo $config->appName; ?></h1>
+<p><strong><?php echo $config->appName; ?> (<?php echo $config->appServerBaseUrl; ?>, <?php echo date('d F Y') ?>).
 Florida State University, Department of Scientific Computing, Tallahassee, FL 32306-4026 USA.<br /></strong></p>
+
+
