68ec1fc6d69f523de180d3eb8d31ab7ddd57f1ef
FSU Institute for Digital Information and Scientific Communication <griccardi@fsu.edu>
2011-08-03 17:33:59 +0000

    added support for morphbank ids

--- www/includes/getIdFromExtId.php
+++ www/includes/getIdFromExtId.php
@@ -3,6 +3,26 @@
 // lookup external id and return local id
 
 function getIdFromExtId($extId){
+	// check for morphbank id
+	$comps = parse_url($extId);
+	if ($comps['host']=='www.morphbank.net'){
+		// morphbank url
+		$query = $comps['query'];
+		$path = $comps['path'];
+		// sample (a) http://www.morphbank.net/12345 ?id=12345
+		$intPath = intval($path);
+		if ($intPath){
+			return $intPath;
+		}
+		// sample (b) http://www.morphbank.net/?id=12345
+		parse_str($query);
+		$intId = intval($id);
+		if(intval($intId)){
+			return $intId;
+		}
+
+	}
+	// check for external id
 	$db = connect();
 	$getIdSelect = "select mbId from ExternalLinkObject where externalId=?";
 	$param_types = array('text');
