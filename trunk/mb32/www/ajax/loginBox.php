87025fad687742b76d9a2c8b5d7168b2317ceae6
rbruhn <rbruhn@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2011-09-08 13:54:19 +0000

    Added id to form for username. Used to set focus on input.

--- trunk/mb32/www/ajax/loginBox.php
+++ trunk/mb32/www/ajax/loginBox.php
@@ -27,7 +27,7 @@ include_once('head.inc.php');
 <form name = "loginForm" action="#" method="post" onsubmit="return false;">
 	<span id="loginMessage"></span>
     <h2>User Name:</h2><br />
-	<input type="text" name="username" size="30" />
+	<input type="text" name="username" id="username" size="30" />
     <br /><br />
 	<h2>Password:</h2><br />
     <input type="password" name="password" size="30"  onkeyup="checkEnterKey(event, 0)" /><br /><br />
