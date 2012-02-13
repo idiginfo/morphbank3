754e6b5b5c7bc0cb02e159a940e9d8967bfdea8e
cmonnom <cmonnom@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2012-02-13 18:36:49 +0000

    test of a new page to upload image files

--- trunk/mb32/www/Submit/uploadImages.php
+++ trunk/mb32/www/Submit/uploadImages.php
@@ -9,9 +9,44 @@ include_once('head.inc.php');
 
 // The beginnig of HTML
 $title = 'Upload images';
-$includeJavaScript = array();
+$javascript = '
+<script language="javascript" type="text/javascript" src="'.$config->domain.'js/spry/SpryTabbedPanels.js"></script>
+<script language="javascript" type="text/javascript" src="'.$config->domain.'js/swapRowColor.js"></script>
+<script language="javascript" type="text/javascript" src="'.$config->domain.'js/managerCalendar.js"></script>
+<script language="javascript" type="text/javascript" src="'.$config->domain.'js/myManager.js"></script>
 
-initHtml( $title, $includeJavaScript, NULL);
+<style type="text/css">@import url(/style/calandar/aqua/theme.css);</style>
+<style type="text/css">@import url(/style/SpryTabbedPanels.css);</style>
+<script type="text/javascript" src="'.$config->domain.'js/calendar/calendar.js"></script>
+<script type="text/javascript" src="'.$config->domain.'js/calendar/lang/calendar-en.js"></script>
+<script type="text/javascript" src="'.$config->domain.'js/calendar/calendar-setup.js"></script>
+
+<script language="javascript" type="text/javascript">
+	// load the content and filters dynamically from their ajax scripts 
+	window.onload = function () {
+		// This way did not work well, due to the page would only display after the ajax was done loading.
+		// Spry.Utils.updateContent(contentId, contentUrl+"?id='.$tab.'");
+		
+		if ("'.$tab.'" != "imageTab") {
+			document.getElementById("'.$tab.'").className = "TabbedPanelsTabSelected";
+			document.getElementById("imageTab").className = "TabbedPanelsTab";	
+			document.resultForm.currentTab.value="'.$tab.'";	
+			previous = document.getElementById("'.$tab.'"); // this is for the tab
+			previousPage = "'.$config->domain.'MyManager/content.php?id='.$tab.'";
+		}
+		
+		updateImageFilters("'.$tab.'");
+		
+		//var onLoadPage = contentUrl+"?id='.$tab.'";
+		var onLoadPage = "'.$myManagerFormValues.'";
+		setTimeout(function(){changePage(onLoadPage)}, 10 );
+		
+		setFooter();	
+	}
+	
+</script>';
+
+initHtml( $title, $javascriptcript, NULL);
 
 // Add the standard head section to all the HTML output.
 echoHead( false, $title);
