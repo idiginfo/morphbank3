debae7ba76e99765097c9638db582f25cde3f89b
cmonnom <cmonnom@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2012-02-13 18:26:17 +0000

    test of a new page to upload image files

---
+++ www/Submit/uploadImages.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Webpage allowing users to drop image files in their own folder
+ * using Javascript.
+ */
+
+include_once('head.inc.php');
+
+
+
+// Finish with end of HTML
+finishHtml();
+
+?>
