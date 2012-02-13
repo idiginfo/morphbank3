9064908e35ac6de4e61d159176cdb1ade97a708d
cmonnom <cmonnom@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2012-02-13 18:35:10 +0000

    test of a new page to upload image files

--- www/Submit/uploadImages.php
+++ www/Submit/uploadImages.php
@@ -1,16 +1,24 @@
 <?php
 
 /*
- * Webpage allowing users to drop image files in their own folder
+ * Webpage allowing users to drop image files in their personal folder
  * using Javascript.
  */
 
 include_once('head.inc.php');
-initHtml( $title, $javascript, NULL);
+
+// The beginnig of HTML
+$title = 'Upload images';
+$includeJavaScript = array();
+
+initHtml( $title, $includeJavaScript, NULL);
 
 // Add the standard head section to all the HTML output.
 echoHead( false, $title);
 
+echo '<div class="mainGenericContainer" style="width:700px">';
+//TODO add the uploader here
+echo '</div>';
 
 // Finish with end of HTML
 finishHtml();
