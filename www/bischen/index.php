33ed0b294b8615ba8678846e47eb14571bbf3756
cmonnom <cmonnom@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2012-04-06 20:37:05 +0000

    test IIP Viewer

--- www/bischen/index.php
+++ www/bischen/index.php
@@ -23,17 +23,18 @@
 
 include_once('head.inc.php');
 include_once('urlFunctions.php');
-include_once('viewFrame.php');
+
 $id = $_GET['id'];
 $sessionId = $_GET['sessionId'];
 if (empty($sessionId)) $sessionId = session_id();
-
+include_once('viewFrame.php');
 // The beginnig of HTML
 $title = "Zooming Viewer for $id";
 initHtml( $title, NULL, NULL);
 
 // Add the standard head section to all the HTML output.
 echoHead( false, $title);
+ViewIipFrame($id);
 echo imageServerTpcFrame($id, 800,600, $sessionId);
 echo "<br/>";
 //echo bischenAttributionTag();
