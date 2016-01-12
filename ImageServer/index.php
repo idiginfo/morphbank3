fd55c1f9be9f9acc9e340485e56369f3f4be50f8
griccardi <griccardi@fsu.edu>
2016-01-12 13:44:23 -0500

    added error log entry for missing  file in index.php

--- ImageServer/index.php
+++ ImageServer/index.php
@@ -21,31 +21,31 @@
  *   Stephen Winner - initial API and implementation
  */
 
-define('PHP_ENTRY',0);// valid Web app entry point
+define('PHP_ENTRY', 0);// valid Web app entry point
 
 include_once('checkAuthorization.php');
 
 $requestServer = $_SERVER['REMOTE_ADDR'];
 $responseServer = $_SERVER['SERVER_NAME'];
-if($requestServer == $responseServer) { // request is from ME!
-	echo "don't call me again like this! $requestServer is calling $responseServer\n";
-	exit;
+if ($requestServer == $responseServer) { // request is from ME!
+    echo "don't call me again like this! $requestServer is calling $responseServer\n";
+    exit;
 }
 
 // Get object id from parameters, if available
 $id = $_REQUEST['id'];
-if (!intval($id)){// id is not an integer, must be a URI
-	// get id from web server
-	$uri = $id;
-	$id = getIdFromURI($uri);
-	if (empty($id)){
-		header("HTTP/1.1 404 Not Found: No image with id '$uri'");
-		return;
-	}
+if (!intval($id)) {// id is not an integer, must be a URI
+    // get id from web server
+    $uri = $id;
+    $id = getIdFromURI($uri);
+    if (empty($id)) {
+        header("HTTP/1.1 404 Not Found: No image with id '$uri'");
+        return;
+    }
 }
 
 $imgType = strtolower($_REQUEST['imgType']);
-if ($imgType=='tif') $imgType = 'tiff';
+if ($imgType == 'tif') $imgType = 'tiff';
 
 $imgSize = $_REQUEST['imgSize'];
 
@@ -54,64 +54,65 @@ $sessionId = $_REQUEST['sessionId'];
 $requestor = $_SERVER['REMOTE_ADDR'];
 
 // for testing purposes
-if (empty($id) || empty($imgType)){
-	header("HTTP/1.1 400 Bad Request");
-	return;
+if (empty($id) || empty($imgType)) {
+    header("HTTP/1.1 400 Bad Request");
+    return;
 }
 
 if (!empty($id)) {
-	// request is for an image file
-	include_once('image.class.php');
-	$image = new Image($id, $imgType, $imgSize, $sessionId);
-        //file cannot be found on the server
-        if(!$image->getFileExists()) {
-            header("HTTP/1.1 404 Not Found: The requested object is not on the server");
+    // request is for an image file
+    include_once('image.class.php');
+    $image = new Image($id, $imgType, $imgSize, $sessionId);
+    //file cannot be found on the server
+    if (!$image->getFileExists()) {
+        error_log("file not found: " . $image->getImageFilePath());
+        header("HTTP/1.1 404 Not Found: The requested object is not on the server");
+        exit();
+    }
+    if (imageRequesterIsBot() && !$image->allowedBotImages()) {
+        header("HTTP/1.1 403 Forbidden");
+        exit();
+    }
+    //TODO test for authorization
+    if (serverRequest($requestor) || $image->getAuthorized() === true) {
+        if (!isModifiedSince($image->getFileTime())) {
+            header("HTTP/1.1 304 Not Modified");
             exit();
         }
-	if(imageRequesterIsBot() && !$image->allowedBotImages()){
-		header("HTTP/1.1 403 Forbidden");
-		exit();
-	}
-	//TODO test for authorization
-	if (serverRequest($requestor) || $image->getAuthorized()===true){
-		if (!isModifiedSince($image->getFileTime())) {
-			header("HTTP/1.1 304 Not Modified");
-			exit();
-		}
 
-		$imageShowed = $image->showImage();
-		if ($imageShowed) return;
-	}
+        $imageShowed = $image->showImage();
+        if ($imageShowed) return;
+    }
 }
 
 //TODO respond to unauthorized image or no image id
 
-if ( $image->getAuthorized()!=true){
-header('Content-Type: image/png');
-readfile($config->webPath.'/style/webImages/'.$config->imgPrivate);
-return;
+if ($image->getAuthorized() != true) {
+    header('Content-Type: image/png');
+    readfile($config->webPath . '/style/webImages/' . $config->imgPrivate);
+    return;
 }
 if (empty($config->imgServerAltUrl) || serverRequest($requestor)) {
-	// no one else to ask
-	header("HTTP/1.1 401 Unauthorized");
-	return;
+    // no one else to ask
+    header("HTTP/1.1 401 Unauthorized");
+    return;
 }
 
 
 //TODO issue proper error message: not authorized
 header('Content-Type: image/png');
-readfile($config->webPath.'/style/webImages/'.$config->imgPrivate);
+readfile($config->webPath . '/style/webImages/' . $config->imgPrivate);
 
 function isModifiedSince($fileTime) {
-	if(!array_key_exists("HTTP_IF_MODIFIED_SINCE", $_SERVER)) return true;
-	$requestModTime = strtotime(preg_replace('/;.*$/','',$_SERVER["HTTP_IF_MODIFIED_SINCE"]));
-	return $requestModTime < $fileTime;
+    if (!array_key_exists("HTTP_IF_MODIFIED_SINCE", $_SERVER)) return true;
+    $requestModTime = strtotime(preg_replace('/;.*$/', '', $_SERVER["HTTP_IF_MODIFIED_SINCE"]));
+    return $requestModTime < $fileTime;
 }
 
-function serverRequest($serverName){
-	global $config;
-	if ($serverName==$config->imgServer) return true;
-	return false;
+function serverRequest($serverName) {
+    global $config;
+    if ($serverName == $config->imgServer) return true;
+    return false;
 }
 
 
