53714d08269545475688c4dfd3dd31606c9ef1dc
Robert <hb7dXEXggo6)Qq%EGs>
2021-06-23 14:51:08 -0400

    Disable login form

--- www/Submit/loginForm.php
+++ www/Submit/loginForm.php
@@ -19,11 +19,12 @@
 *   Katja Seltmann - initial API and implementation
 *   Stephen Winner - initial API and implementation
 -->
-
+<p>Morphbank is in the process of being retired. All account functions have been disabled.</p>
+<!--
 <form name="login" id="frmLogin" action="login.php" method="post">
 <table border="0" cellpadding="5">
 	<tr>
-		<td colspan="3" align="right"><h1><strong><?php echo $config->appName; ?> Login</strong></h1><br /><br /></td>
+		<td colspan="3" align="right"><h1><strong><?php //echo $config->appName; ?> Login</strong></h1><br /><br /></td>
 	</tr>
 	<tr>
 		<td align="right"><h2>User Name:</h2> <span class="req">*</span></td>
@@ -34,9 +35,10 @@
 		<td colspan="2"><input type="password" name="password" size="30" /></td>
 	</tr>
 </table>
-<?php echo frmSubmitButton('Submit'); ?>
+<?php //echo frmSubmitButton('Submit'); ?>
 <br /> <br />
 <h3>Request a user account</h3> <a href="/Admin/User/new">Click Here</a> 
 <br/><br/>
 <h3>Forgot your username/password?</h3> <a href="/Admin/User/userpassword.php">Click here</a>		
 </form>
+-->
